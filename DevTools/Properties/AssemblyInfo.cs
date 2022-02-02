ed65b99b5fc36ccddc8b8d484d8722577787fbb6
KatilynnStarfinder <katilynn@secondgalaxy.com>
2022-02-02 11:52:42 -0500

    Initial commit of source code
    - This source code is taken under the open source license found in the IslandzVW/halcyon repository on Github.  Additional updates were merged in from the HalcyonGrid/halcyon repository also found on Github.  This is not a fork of the HalcyonGrid version and is the source code direct from the IslandsVW/halcyon repository.
    
    - We thank InWorldz LLC and their team of developers for the hard work they put into the Halcyon Platform.  It is the honor of the team at Virtual World Research Inc. to continue moving our version of this platform forward under the Hyperion Legacy project.
    
    - Additional credit goes to the work done by the developers of the HalcyonGrid project for their hard work.

---
+++ DevTools/Properties/AssemblyInfo.cs
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) Contributors, https://hyperionvirtual.com
+ * Copyright (c) Virtual World Research Inc.
+ * Copyright (c) Halcyon Grid Developers
+ * Copyright (c) InWorldz Halcyon Developers
+ * Copyright (c) Contributors, http://opensimulator.org/
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Hyperion Legacy Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+/// <summary>
+/// General Information
+/// 
+/// The general information about an assembly is
+/// controlled through the following set of attributes.
+/// We change these attribute values to modify the
+/// information associated with an assembly.
+/// </summary>
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+/// <summary>
+/// ComVisible Settings
+/// 
+/// By setting the ComVisible to false we
+/// make the types in this assembly not visible
+/// to COM components.  If we need to access
+/// a type in this assembly from COM, we set the
+/// ComVisible attribute to true on that type.
+/// </summary>
+[assembly: ComVisible(false)]
+
+/// <summary>
+/// GUID Information
+/// 
+/// The following GUID is for the ID of the
+/// typelib if this project is exposed to COM.
+/// </summary>
+[assembly: Guid("")]
+
+/// <summary>
+/// Version Information
+/// 
+/// The version information for an assembly
+/// consists of the following four values:
+/// 
+///     Major Version
+///     Minor Version
+///     Build Number
+///     Revision Number
+///     
+/// We can set the Build and Revision Numbers
+/// manually or we can default the values by
+/// using "*" as follows:
+/// 
+///     [assembly: AssemblyVersion("1.0.0.*")]
+/// </summary>
+[assembly : AssemblyVersion("")]
+[assembly : AssemblyFileVersion("")]
